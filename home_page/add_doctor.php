4fe86d6ead26d7140a21129659d85303c4be2166
Khaled Mostafa <95879454+El3aQeeD@users.noreply.github.com>
2023-02-12 00:42:00 +0200

    edits in add_doctor file

--- home_page/add_doctor.php
+++ home_page/add_doctor.php
@@ -3,14 +3,14 @@
 include '../connect.php';
 
 $name=filterRequest("name");
-$doctorSpecialtyId=filterRequest("doctorSpecialtyId");
+$doctorSpecialty=filterRequest("doctorSpecialty");
 $doctorAbout=filterRequest("doctorAbout");
 $image=imageUpload("doctorImageFile");
 
 if($image != 'fail')
 {
-    $stmt= $con->prepare("INSERT INTO doctors (doctor_name,doctor_specialty_id,doctor_about,doctor_image) VALUES (?,?,?,?)");
-    $stmt->execute(array($name,$doctorSpecialtyId,$doctorAbout,$image));
+    $stmt= $con->prepare("INSERT INTO doctors (doctor_name,doctor_specialty,doctor_about,doctor_image) VALUES (?,?,?,?)");
+    $stmt->execute(array($name,$doctorSpecialty,$doctorAbout,$image));
     $count=$stmt->rowCount();
     
     if($count>0)
