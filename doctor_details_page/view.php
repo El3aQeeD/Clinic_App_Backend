28aeadbe2a5e445271020f83f6e327eb7e27da1c
hany170 <100557057+hany170@users.noreply.github.com>
2023-02-13 07:17:08 +0200

    doctor doctor_details_page

---
+++ doctor_details_page/view.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../connect.php';
+$id=filterRequest("id");
+
+$stmt= $con->prepare("SELECT * FROM doctors WHERE `doctor_id`= ?");
+$stmt->execute(array($id));
+$count=$stmt->rowCount();
+$data=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if($count>0)
+{
+    echo json_encode($data);
+}
+else
+{
+    echo json_encode(array("status"=>"fail"));
+}
+
+?>
